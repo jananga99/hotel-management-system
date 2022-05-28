7329d463f75336e0bc72628c9f0471f17de2d0d2
KabilanMA <kabilanen@gmail.com>
2022-05-28 19:38:26 +0530

    Deployment Test 2

--- backend/models/database/connection.js
+++ backend/models/database/connection.js
@@ -1,16 +1,31 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASS,
-    database: process.env.DATABASE_NAME,
-    port: process.env.DATABASE_PORT
-});
+var con;
+function handleDisconnect() {
+    con = mysql.createConnection({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
+    });
 
-con.connect(
-    (err)=>{
-    if(err) throw err;
-});
+    con.connect((err) => {
+        if(err) {
+            console.log('error when connecting to db: ', err);
+            setTimeout(handleDisconnect, 2000);
+        }
+    });
+
+    con.on('error', (err)=>{
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            handleDisconnect();
+        }else {
+            throw err;
+        }
+    });
+}
+
+handleDisconnect();
 
 module.exports = con;
