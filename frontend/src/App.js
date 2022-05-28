6e4e8aebe0abc85dfe39d2c7dfd5c545ad9ed08c
KabilanMA <kabilanen@gmail.com>
2022-05-28 13:39:25 +0530

    Bug Fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,11 +15,11 @@ import CreateCustomer from "./components/admin/CreateCustomer";
 import BookingAllHotels from "./components/booking/AllHotels";
 import BookingHotel from "./components/booking/Hotel";
 import Booking from "./components/booking/Book";
-import ModeratorDashboard from "./components/Moderator/ModeratorDashboard";
-import AllRooms from "./components/Moderator/AllRooms";
-import CreateRoom from "./components/Moderator/CreateRoom";
-import CreateHotel from "./components/Moderator/hotel_form";
-import ViewHotel from "./components/Moderator/view_Hotel";
+import ModeratorDashboard from "./components/moderator/ModeratorDashboard";
+import AllRooms from "./components/moderator/AllRooms";
+import CreateRoom from "./components/moderator/CreateRoom";
+import CreateHotel from "./components/moderator/hotel_form";
+import ViewHotel from "./components/moderator/view_Hotel";
 import SignupForm from "./components/admin/SignupForm";
 import Interface from "./components/Interface";
 
@@ -33,8 +33,8 @@ class App extends React.Component {
       let res = await fetch("/isLoggedIn", {
         method: "POST",
         headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
         },
       });
 
@@ -61,8 +61,8 @@ class App extends React.Component {
       let res = await fetch("/logout", {
         method: "POST",
         headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
         },
       });
 
--- frontend/src/components/LoginForm.js
+++ frontend/src/components/LoginForm.js
@@ -54,7 +54,7 @@ class LoginForm extends React.Component {
             let result = await res.json();
 
             if(result && result.success) {
-                UserStore.modifyObservable(false, true, result.first_name, result.email, result.type)
+                UserStore.modifyObservable(false, true, result.first_name, result.email, result.type);
             } else if(result && result.success === false) {
                 this.resetForm();
                 alert(result.msg);
